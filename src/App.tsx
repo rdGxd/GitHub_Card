b3fceb5d57769a312d5486e21f9ad13ef36a1ed4
Rodrigo <rdg.hora1@gmail.com>
2023-12-27 10:34:15 -0300

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import reactLogo from "./assets/react.svg";
+
 function App() {
   return (
     <>
-      <h1 className="">Oi</h1>
+      <h1>Oi</h1>
+      <img src={reactLogo} alt="" />
     </>
   );
 }
