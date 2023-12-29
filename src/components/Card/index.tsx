6e04c7c756aec8422d4dcdee57ba24c1f73640ef
Rodrigo Silva <rdg.hora1@gmail.com>
2023-12-29 08:55:48 -0300

    image Profile

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getData } from "../../api/getData";
+import seguidores from "../../assets/Icons/Seguidores.svg";
+import logo from "../../assets/Icons/logo.svg";
 import { DataProps } from "../../types/data";
 
 export const Card = () => {
@@ -24,24 +26,27 @@ export const Card = () => {
       <div className="flex justify-center">
         <div className="w-10/12 h-screen rounded-xl bg-white flex justify-center items-center ">
           {data && (
-            <div className="w-10/12 h-[90%] bg-card rounded-xl flex">
-              <div className="flex mt-9 ml-8 ">
-                <img
-                  src={data.avatar_url}
-                  alt="Avatar GitHub"
-                  className="w-9 h-9 rounded-full mr-5"
-                />
-                <p className="mt-2">{data.login}</p>
-              </div>
+            <div className="w-10/12 h-[90%] bg-card rounded-xl flex flex-wrap">
+              <header className="HEADER flex mt-9 ml-4">
+                <div className="rounded-full mr-3 border w-12 h-12 flex items-center justify-center">
+                  <img src={logo} alt="logo" className="" />
+                </div>
+                <p className="mt-3">{data.login}</p>
+              </header>
 
-              <div className="flex flex-wrap mt-10 content-center ">
-                <div className="flex  rounded-full border-8 border-img-profile w-80">
+              <div className="IMAGE-PROFILE flex flex-wrap content-center">
+                <div className="flex  rounded-full border-8 border-img-profile ">
                   <img
                     src={data.avatar_url}
                     alt=""
-                    className="rounded-full w-full"
+                    className="rounded-full w-96"
                   />
                 </div>
+              </div>
+
+              <div className="INFO flex flex-wrap bg-gradient-to-t from-slate-300">
+                <img src={seguidores} alt="" />
+                <p>Seguidores</p>
               </div>
             </div>
           )}
